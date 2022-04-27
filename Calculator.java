0b4dd81010e9e74be07db4cec4f4d0ec3609526d
jasminsaheed <beegumjasmin7@gmail.com>
2022-04-27 09:00:48 +0530

    initialize

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+class Calculator
+{
+	
+public static void add()
+{
+int a=20;
+int b=10;
+int c;
+c=a+b;
+System.out.println(c);
+}
+public static void sub()
+{
+	int d=10;
+	int e=5;
+	int f;
+	f=d-e;
+	System.out.println(f);
+}
+public static void multiply()
+{
+int a=20;
+int b=10;
+int c;
+c=a*b;
+System.out.println(c);
+}
+public static void division()
+{
+int a=20;
+int b=10;
+int c;
+c=a/b;
+System.out.println(c);
+}
+public static void main(String args[])
+{
+Calculator.add();
+Calculator.sub();
+Calculator.multiply();
+Calculator.division();
+}
+}
