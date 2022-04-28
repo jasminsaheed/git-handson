84c00b8492b31324e37a1a256764f38202990731
jasminsaheed <beegumjasmin7@gmail.com>
2022-04-28 08:37:59 +0530

    java

---
+++ Bank.java
@@ -0,0 +1,23 @@
+class Bank
+{static int amount,wamount,bal;
+public static int deposit( int a)
+{
+amount=a;
+return amount;}
+public static int withdraw(int b)
+{
+wamount=b;
+return wamount;}
+public static void account()
+{
+bal=amount-wamount;
+
+System.out.println(bal);
+}
+public static void main(String args[])
+{
+int p=Bank.deposit(5000);
+System.out.println("deposit amount is" +p);
+int q=Bank.withdraw(1000);
+System.out.println("withdraw amount is" +q);
+Bank.account();}}
