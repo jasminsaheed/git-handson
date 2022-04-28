335da7999e936d7ad31be5bfff82c1d9927c0915
jasminsaheed <beegumjasmin7@gmail.com>
2022-04-28 08:52:42 +0530

    changes

--- Bank.java
+++ Bank.java
@@ -16,7 +16,7 @@ System.out.println(bal);
 }
 public static void main(String args[])
 {
-int p=Bank.deposit(5000);
+int p=Bank.deposit(6000);
 System.out.println("deposit amount is" +p);
 int q=Bank.withdraw(1000);
 System.out.println("withdraw amount is" +q);
