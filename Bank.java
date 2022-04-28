1d0cf7771a74e2f3e5d6cc62c9add5f0c7580585
jasminsaheed <104408539+jasminsaheed@users.noreply.github.com>
2022-04-28 08:45:46 +0530

    pgm updated

--- Bank.java
+++ Bank.java
@@ -17,7 +17,7 @@ System.out.println(bal);
 public static void main(String args[])
 {
 int p=Bank.deposit(5000);
-System.out.println("deposit amount is" +p);
+System.out.println("deposited  amount is" +p);
 int q=Bank.withdraw(1000);
-System.out.println("withdraw amount is" +q);
+System.out.println("withdrawed amount is" +q);
 Bank.account();}}
